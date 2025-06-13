97fe8b36875786e052db225bc097c2baa44142e1
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-06-13 09:31:21 -0400

    Hacerlo responsivo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostListener, ElementRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuAbierto = false;
+
+  constructor(private elementRef: ElementRef) {}
+
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  cerrarMenu() {
+    this.menuAbierto = false;
+  }
+
+  // Detectar clic fuera del navbar
+@HostListener('document:click', ['$event'])
+onClickFuera(event: MouseEvent) {
+  const clicDentro = this.elementRef.nativeElement.contains(event.target);
+  if (!clicDentro && this.menuAbierto) {
+    this.menuAbierto = false;
+  }
+}
+
+}
